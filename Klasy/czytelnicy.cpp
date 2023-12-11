9d1026c966ee6ffecdca37864ae9afba6897542a
DmitriyLevitskiy <107146171+DmitriyLevitskiy@users.noreply.github.com>
2023-12-11 19:29:38 +0100

    Add files via upload

---
+++ Klasy/czytelnicy.cpp
@@ -0,0 +1,22 @@
+﻿#include "czytelnicy.h"
+#include <iostream>
+using namespace std;
+void Czytelnicy::powiadomOZblizajacymSieTerminie(int dni) {
+    // Realizacja metody powiadamiania o zbliżającym się terminie zwrotu książki
+}
+
+void Czytelnicy::wyszukajDostepneKsiazki(const string& tytul) {
+    // Realizacja metody wyszukiwania dostępnych książeczek
+}
+
+void Czytelnicy::wypozyczKsiazke(const string& tytul) {
+    // Realizacja metody wypożyczania książek
+}
+
+void Czytelnicy::zwrocKsiazke(const string& tytul) {
+    // Realizacja metody zwrotu książki
+}
+
+void Czytelnicy::wyswietlSkonczoneCzasyWypozyczen() {
+    // Realizacja metody wyświetlania wygasłych terminów wypozyczen
+}
---
+++ Klasy/pracownicy.cpp
@@ -0,0 +1,30 @@
+﻿#include "pracownicy.h"
+#include <iostream>
+using namespace std;
+
+void Pracownicy::dodajKsiazke(const KsiazkiWObiegu& ksiazka) {
+    listaKsiazek.push_back(ksiazka);
+}
+
+void Pracownicy::usunKsiazke(const KsiazkiWObiegu& ksiazka) {
+    // Załóżmy, że KsiazkiWObiegu mają przeciążony operator porównania do sprawdzania równości
+    auto it = std::find(listaKsiazek.begin(), listaKsiazek.end(), ksiazka);
+    if (it != listaKsiazek.end()) {
+        listaKsiazek.erase(it);
+    }
+}
+
+void Pracownicy::dodajUzytkownika(const Czytelnicy& czytelnik) {
+    listaCzytelnikow.push_back(czytelnik);
+    // Realizacja metody dodawania użytkownika
+}
+
+void Pracownicy::wyswietlCzasyWypozyczen() {
+    // Realizacja metody wyświetlania wygasłych terminów wypozyczen
+}
+
+void Pracownicy::wyswietlSkonczoneCzasyWypozyczen() {
+    for (auto& czytelnik : listaCzytelnikow) {
+        czytelnik.wyswietlSkonczoneCzasyWypozyczen();
+    }
+}
