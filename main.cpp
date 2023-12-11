140e80a42a1a61f64b23f0f411a10206aea785bd
DmitriyLevitskiy <107146171+DmitriyLevitskiy@users.noreply.github.com>
2023-12-11 19:30:17 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+﻿#include "pracownicy.h"
+#include "czytelnicy.h"
+#include "ksiazkiwobiegu.h"
+using namespace std;
+int main() {
+    // Tworzenie obiektów klasy
+    Pracownicy pracownicy;
+    Czytelnicy czytelnicy;
+    KsiazkiWObiegu ksiazkiWObiegu;
+
+    // Przykład zastosowania metod
+    pracownicy.dodajKsiazke("Wiedźmin", 10);
+    pracownicy.usunKsiazke("Hobbit");
+    pracownicy.dodajUzytkownika("Jan Kowalski");
+    pracownicy.wyswietlCzasyWypozyczen();
+
+    czytelnicy.powiadomOZblizajacymSieTerminie(7);
+    czytelnicy.wyszukajDostepneKsiazki("Wiedźmin");
+    czytelnicy.wypozyczKsiazke("Wiedźmin");
+    czytelnicy.zwrocKsiazke("Wiedźmin");
+
+    ksiazkiWObiegu.zmienIlosc("Wiedźmin", 5);
+    ksiazkiWObiegu.wypozycz("Wiedźmin");
+    ksiazkiWObiegu.zwroc("Wiedźmin");
+
+    return 0;
+}
