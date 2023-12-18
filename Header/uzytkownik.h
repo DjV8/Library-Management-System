75be9164b1fcfdb92554f180a33f947280968b07
DjV8 <bartekkreski@gmail.com>
2023-12-18 11:01:51 +0100

    Uzupełnienie klasy Uzytkownik

--- Header/uzytkownik.h
+++ Header/uzytkownik.h
@@ -5,12 +5,19 @@ using namespace std;
 
 class Uzytkownik
 {
-public:
-	Uzytkownik();
-	Uzytkownik(string login, string haslo, string ID);
-
-private:
 	string login;
 	string haslo;
-	string ID;
+	string id;
+public:
+	Uzytkownik();
+	Uzytkownik(string login, string haslo, string id);
+
+	string get_login();
+	string get_haslo();
+	string get_id();
+
+	void zmien_login(string login);
+	void zmien_haslo(string haslo);
+	void zmien_id(string id);
+
 };
--- Klasy/uzytkownik.cpp
+++ Klasy/uzytkownik.cpp
@@ -4,13 +4,21 @@ Uzytkownik::Uzytkownik()
 {
 	this->login = "";
 	this->haslo = "";
-	this->ID = "";
+	this->id = "";
 }
 
-Uzytkownik::Uzytkownik(string login, string haslo, string ID)
+Uzytkownik::Uzytkownik(string login, string haslo, string id)
 {
 	this->login = login;
 	this->haslo = haslo;
-	this->ID = ID;
+	this->id = id;
 }
+
+string Uzytkownik::get_login() { return login; }
+string Uzytkownik::get_haslo() { return haslo; }
+string Uzytkownik::get_id() { return id; }
+
+void Uzytkownik::zmien_login(string login) { this->login = login; }
+void Uzytkownik::zmien_haslo(string haslo) { this->haslo = haslo; }
+void Uzytkownik::zmien_id(string id) { this->id = id; }
 
