decd4683000af12e8ed5176eb69ff152d88e9a01
DmitriyLevitskiy <107146171+DmitriyLevitskiy@users.noreply.github.com>
2024-01-24 21:58:50 +0100

    Update czytelnik.h

--- Header/czytelnik.h
+++ Header/czytelnik.h
@@ -11,11 +11,16 @@ public:
 	Czytelnik();
 	Czytelnik(string login, string haslo, string id);
 
+	//void powiadomOZblizajacymSieTerminie(int dni);
 	void powiadomOZblizajacymSieTerminie(int dni);
-	void wyszukajDostepneKsiazki(const string& tytul);
-	void wypozyczKsiazke(const string& tytul);
-	void zwrocKsiazke(const string& tytul);
-	void wyswietlSkonczoneCzasyWypozyczen();
+	//void wyszukajDostepneKsiazki(const string& tytul);
+	void wyszukajDostepneKsiazki(const string& tytul, const Database& db) const;
+	//void wypozyczKsiazke(const string& tytul);
+	void wypozyczKsiazke(const string& tytul, Database& db);
+	//void zwrocKsiazke(const string& tytul);
+	void zwrocKsiazke(const string& tytul, Database& db);
+	//void wyswietlSkonczoneCzasyWypozyczen();
+	void wyswietlSkonczoneCzasyWypozyczen(Database& db);
 
 	friend ostream& operator<<(ostream& out, Czytelnik& b);
 	friend istream& operator>>(istream& in, Czytelnik& b);
