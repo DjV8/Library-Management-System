d7c166840989d47c9e27be2845b5ea50adc77f19
DjV8 <bartekkreski@gmail.com>
2024-01-27 19:57:00 +0100

    zmiana nazwy funkcji w "Książka w obiegu"

--- Klasy/pracownik.cpp
+++ Klasy/pracownik.cpp
@@ -54,27 +54,8 @@ istream& operator>>(istream& in, Pracownik& p) {
 //==================================zmieniłem
 
 void Pracownik::dodajKsiazke(Database& a, const Ksiazka k) {
-    a.getKsiazki()->push(k);
-    cout << "Ksiazka dodana do bazy danych." << endl;
-}
-
-void Pracownik::usunKsiazke(Database& a, Ksiazka k) {
-    int vector_size = a.getKsiazki()->size();
-    for (int i = 0; i < vector_size; i++) {
-        Ksiazka ak = a.getKsiazki()->get(i);
-        if (ak == k) {
-            a.getKsiazki()->remove(i);
-            cout << "Ksiazka usunięta z bazy danych." << endl;
-            return;
-        }
-    }
-    cout << "Ksiazka nie znaleziona w bazie danych." << endl;
-}
-
-/*
-void Pracownik::dodajKsiazke(Database& a, const Ksiazka k)
-{
 	a.getKsiazki()->push(k);
+	cout << "Ksiazka dodana do bazy danych." << endl;
 }
 
 void Pracownik::usunKsiazke(Database& a, Ksiazka k) {
@@ -83,47 +64,37 @@ void Pracownik::usunKsiazke(Database& a, Ksiazka k) {
 		Ksiazka ak = a.getKsiazki()->get(i);
 		if (ak == k) {
 			a.getKsiazki()->remove(i);
+			cout << "Ksiazka usunięta z bazy danych." << endl;
 			return;
 		}
 	}
+	cout << "Ksiazka nie znaleziona w bazie danych." << endl;
 }
-*/
+
 //==================================================
 
 void Pracownik::dodajCzytelnik(Database& a, const Czytelnik czytelnik) {
 	a.getCzytelnicy()->push(czytelnik);
 	// Realizacja metody dodawania czytelnika
 }
-/*
-void Pracownik::wyswietlCzasyWypozyczen() {
-	// Realizacja metody wyświetlania wygasłych terminów wypozyczen
-}
-*/
+
 //=========================================zmieniłem
 void Pracownik::wyswietlSkonczoneCzasyWypozyczen(Database& a) {
-    cout << "Wyświetlanie skończonych czasów wypożyczeń:" << endl;
-    
-    for (int i = 0; i < a.getKsiazkiWObiegu()->size(); ++i) {
-        Ksiazka_W_Obiegu ko = a.getKsiazkiWObiegu()->get(i);
+	cout << "Wyświetlanie skończonych czasów wypożyczeń:" << endl;
 
-        string ISBN = ko.getISBN();
-        int czas_wypozyczenia = ko.getCzasWypozyczenia();
-        int czas_zakonczenia = ko.getCzasZakonczenia();
+	for (int i = 0; i < a.getKsiazkiWObiegu()->size(); ++i) {
+		Ksiazka_W_Obiegu ko = a.getKsiazkiWObiegu()->get(i);
 
-        if (czas_zakonczenia < time(nullptr)) {
-            cout << "ISBN: " << ISBN << endl;
-            cout << "Czas wypożyczenia: " << czas_wypozyczenia << endl;
-            cout << "Czas zakończenia wypożyczenia: " << czas_zakonczenia << endl;
+		string ISBN = ko.get_ISBN();
+		int czas_wypozyczenia = ko.get_czas_wyporzczenia();
+		int czas_zakonczenia = ko.get_koniec_czasu_wyporzczenia();
 
-            cout << endl;
-        }
-    }
-}
-//=================================================
-void Pracownik::wyswietlSkonczoneCzasyWypozyczen() {
-	/*
-	for (auto& czytelnik : listaCzytelnikow) {
-		czytelnik.wyswietlSkonczoneCzasyWypozyczen();
+		if (czas_zakonczenia < time(nullptr)) {
+			cout << "ISBN: " << ISBN << endl;
+			cout << "Czas wypożyczenia: " << czas_wypozyczenia << endl;
+			cout << "Czas zakończenia wypożyczenia: " << czas_zakonczenia << endl;
+
+			cout << endl;
+		}
 	}
-	*/
 }
