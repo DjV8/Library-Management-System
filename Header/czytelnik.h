4a829f4d473142705b16e37cf7d8256524de3655
DjV8 <bartekkreski@gmail.com>
2024-01-27 19:54:45 +0100

    Młyn

--- Header/czytelnik.h
+++ Header/czytelnik.h
@@ -1,32 +1,41 @@
 ﻿#ifndef CZYTELNICY_H
 #define CZYTELNICY_H
+#pragma once
 
 #include <iostream>
-#include <vector>
-
 #include "uzytkownik.h"
+#include "ksiazka_W_obiegu.h"
+#include "Vector.h"
+
+
 using namespace std;
+
+class Database;
+
 class Czytelnik :public Uzytkownik {
 public:
+	~Czytelnik();
 	Czytelnik();
 	Czytelnik(string login, string haslo, string id);
 
-	//void powiadomOZblizajacymSieTerminie(int dni);
-	void powiadomOZblizajacymSieTerminie(int dni);
-	//void wyszukajDostepneKsiazki(const string& tytul);
-	void wyszukajDostepneKsiazki(const string& tytul, const Database& db) const;
-	//void wypozyczKsiazke(const string& tytul);
-	void wypozyczKsiazke(const string& tytul, Database& db);
-	//void zwrocKsiazke(const string& tytul);
+	void powiadomOZblizajacymSieTerminie(int dni, Database& db);
+	void wyszukajDostepneKsiazki(const string& tytul, Database& db);
+	//Domyślny czas wypożczenia : 2 tyg
+	void wypozyczKsiazke(const string& ISBN, Database& db, int czas_wyporzyczenia = 2 * 7 * 24 * 60 * 60);
 	void zwrocKsiazke(const string& tytul, Database& db);
-	//void wyswietlSkonczoneCzasyWypozyczen();
+
+	void zaladuj_ksiazki_wypozyczone(Database& db);
+
 	void wyswietlSkonczoneCzasyWypozyczen(Database& db);
+	void wyswietlWyporzyczoneKsiazki(Database& db);
+
 
 	friend ostream& operator<<(ostream& out, Czytelnik& b);
 	friend istream& operator>>(istream& in, Czytelnik& b);
 
 private:
-	vector<pair<string, bool>> ksiazkiWypozyczone;
+	//Wskaźnik bo miałem problemy z inicjalizacją
+	Vector<Ksiazka_W_Obiegu*>* ksiazkiWypozyczone = nullptr;
 };
 
 #endif
--- Klasy/czytelnik.cpp
+++ Klasy/czytelnik.cpp
@@ -1,8 +1,26 @@
 ﻿#include "..\Header\czytelnik.h"
+#include "..\Header\ksiazka_W_obiegu.h"
+#include "..\Header\baza_danych.h"
 #include <iostream>
 using namespace std;
 
+
+// Załadowanie wskaźnika na ksiażki w obiegu by były zsynchronizowane z bazą danych
+void Czytelnik::zaladuj_ksiazki_wypozyczone(Database& db) {
+	Vector<Ksiazka_W_Obiegu>* K_W_O = db.getKsiazkiWObiegu();
+	ksiazkiWypozyczone = new Vector<Ksiazka_W_Obiegu*>;
+	for (Ksiazka_W_Obiegu& K : (*K_W_O))
+		if (K.get_id_osoby() == get_id())
+			ksiazkiWypozyczone->push(&K);
+}
+
+Czytelnik::~Czytelnik()
+{
+	delete ksiazkiWypozyczone;
+}
+
 Czytelnik::Czytelnik() : Uzytkownik() {}
+
 Czytelnik::Czytelnik(string login, string haslo, string id) :Uzytkownik(login, haslo, id) {};
 
 ostream& operator<<(ostream& out, Czytelnik& c) {
@@ -49,111 +67,92 @@ istream& operator>>(istream& in, Czytelnik& c) {
 	return in;
 }
 
-/*
-void Czytelnik::powiadomOZblizajacymSieTerminie(int dni) {
-	// Realizacja metody powiadamiania o zbliżającym się terminie zwrotu książki
-}
-*/
+void Czytelnik::powiadomOZblizajacymSieTerminie(int dni, Database& db) {
+	// Pobierz bieżącą datę
+	time_t now = time(nullptr);
 
-void Czytelnik::powiadomOZblizajacymSieTerminie(int dni) {
-    // Pobierz bieżącą datę
-    time_t now = time(nullptr);
+	// Zakładamy, że Czytelnik ma listę książek, które wypożyczył
+	for (Ksiazka_W_Obiegu* KW : *ksiazkiWypozyczone) {
+		bool zwrot = KW->get_zwrot();
 
-    // Zakładamy, że Czytelnik ma listę książek, które wypożyczył
-    for (const auto& para : ksiazkiWypozyczone) {
-        const string& tytul = para.first;
-        bool zwrot = para.second;
+		// Sprawdź, czy książka ma ustalony okres zwrotu
+		if (!zwrot) {
+			int czasZakonczenia = KW->get_koniec_czasu_wyporzczenia();
 
-        // Sprawdź, czy książka ma ustalony okres zwrotu
-        if (!zwrot) {
-            Ksiazka_W_Obiegu* ksiazkaWObiegu = znajdzKsiazkeWObiegu(tytul);  // Załóżmy, że masz w obiegu funkcję wyszukiwania książek
-            if (ksiazkaWObiegu) {
-                int czasZakonczenia = ksiazkaWObiegu->getCzasZakonczenia();
-                
-                // Oblicz, ile dni pozostało do terminu zwrotu.
-                int dniDoZakonczenia = (czasZakonczenia - now) / (24 * 60 * 60);
+			// Oblicz, ile dni pozostało do terminu zwrotu.
+			int dniDoZakonczenia = (czasZakonczenia - now) / (24 * 60 * 60);
 
-                // Sprawdź, czy do terminu zwrotu pozostało mniej niż "dni".
-                if (dniDoZakonczenia <= dni) {
-                    wyslijPowiadomienie("Zbliża się termin zwrotu książki: " + tytul);
-                }
-            }
-        }
-    }
+			// Sprawdź, czy do terminu zwrotu pozostało mniej niż "dni".
+			if (dniDoZakonczenia <= dni) {
+				//wyslijPowiadomienie("Zbliża się termin zwrotu książki: " + tytul);
+				string isbn = KW->get_ISBN();
+				Ksiazka* K = db.get_ksiazka_by_ISBN(isbn);
+				cout << "Zbliża się termin zwrotu książki: " << K->get_tytul();
+			}
+		}
+	}
 }
 
-
-/*
-void Czytelnik::wyszukajDostepneKsiazki(const string& tytul) {
-	// Realizacja metody wyszukiwania dostępnych książeczek
-}
-*/
-void Czytelnik::wyszukajDostepneKsiazki(const string& tytul, const Database& db) const {
-    cout << "Dostępne książki o tytule '" << tytul << "':" << endl;
-
-    for (const auto& ksiazka : db.getKsiazki()) {
-        if (ksiazka.get_tytul() == tytul && ksiazka.get_ilosc() > 0) {
-            cout << ksiazka << endl;
-        }
-    }
+void Czytelnik::wyszukajDostepneKsiazki(const string& tytul, Database& db) {
+	cout << "Dostępne książki o tytule '" << tytul << "':" << endl;
+	Vector<Ksiazka> dostepne_ksiazki = db.get_ksiazki_by_tytul(tytul);
+	dostepne_ksiazki.print();
 }
 
-
-/*
-void Czytelnik::wypozyczKsiazke(const string& tytul) {
-	// Realizacja metody wypożyczania książek
+//Wypożyczamy po ISBN bo tytuł może nie być unikalny
+void Czytelnik::wypozyczKsiazke(const string& ISBN, Database& db, int czas_wyporzyczenia) {
+	Ksiazka* wybrana_ksiazka = db.get_ksiazka_by_ISBN(ISBN);
+	// Jeśli książka istnieje i jest dostępna
+	if (wybrana_ksiazka->get_tytul() != "" && wybrana_ksiazka->get_ilosc() > 0) {
+		//zmniejsz jej ilość i dodaj informację o wydaniu do obiegu
+		wybrana_ksiazka->zmienIlosc(wybrana_ksiazka->get_ilosc() - 1);
+		db.getKsiazkiWObiegu()->push(Ksiazka_W_Obiegu(this->get_id(), wybrana_ksiazka->get_ISBN(), int(time(nullptr)), int(time(nullptr) + czas_wyporzyczenia)));
+		//Synchronizacja wyporzyczonych ksiażek
+		zaladuj_ksiazki_wypozyczone(db);
+		cout << "Ksiazka '" << wybrana_ksiazka->get_tytul() << "' została wypozyczona." << endl;
+	}
+	else {
+		cout << "Ksizaka nie jest dostepna." << endl;
+	}
 }
-*/
-
-void Czytelnik::wypozyczKsiazke(const string& tytul, Database& db) {
-    // Zakłada się, że w Bazie Danych istnieje metoda sprawdzenia dostępności książki
-    if (db.sprawdzDostepnoscKsiazki(tytul)) {
-        // Jeśli książka jest dostępna, zmniejsz jej ilość i dodaj informację o wydaniu do obiegu
-        db.zmniejszIloscKsiazki(tytul);
-        db.dodajKsiazkeWObiegu(tytul, getId());
-        cout << "Książka '" << tytul << "' została wypożyczona." << endl;
-    } else {
-        cout << "Książka '" << tytul << "' nie jest dostępna." << endl;
-    }
+//Zwracamy po ISBN bo tytuł może nie być unikalny
+void Czytelnik::zwrocKsiazke(const string& ISBN, Database& db) {
+	for (Ksiazka_W_Obiegu* KW : *ksiazkiWypozyczone) {
+		bool zwrot = KW->get_zwrot();
+		// Jeśli książka należy do tego czytelnika i jest w obiegu, należy ją mu zwrócić
+		if (!zwrot && KW->get_ISBN() == ISBN) {
+			KW->set_zwrot(true);
+			Ksiazka* wybrana_ksiazka = db.get_ksiazka_by_ISBN(ISBN);
+			wybrana_ksiazka->zmienIlosc(wybrana_ksiazka->get_ilosc() + 1);
+			cout << "Ksiazka '" << wybrana_ksiazka->get_tytul() << "' została zwrocona." << endl;
+			return;
+		}
+	}
+	cout << "Nie mozesz zwrocic ksiazki '" << "'." << endl;
 }
-
-
-/*
-void Czytelnik::zwrocKsiazke(const string& tytul) {
-	// Realizacja metody zwrotu książki
-}
-*/
-
-void Czytelnik::zwrocKsiazke(const string& tytul, Database& db) {
-
-    if (db.sprawdzObecnoscKsiazki(tytul, getId())) {
-        // Jeśli książka należy do tego czytelnika i jest w obiegu, należy ją mu zwrócić
-        db.usunKsiazkeWObiegu(tytul);
-        db.zwiekszIloscKsiazki(tytul);
-        cout << "Książka '" << tytul << "' została zwrócona." << endl;
-    } else {
-        cout << "Nie możesz zwrócić książki '" << tytul << "'." << endl;
-    }
-}
-
-
-/*
-void Czytelnik::wyswietlSkonczoneCzasyWypozyczen() {
-	// Realizacja metody wyświetlania wygasłych terminów wypozyczen
-}
-*/
 
 void Czytelnik::wyswietlSkonczoneCzasyWypozyczen(Database& db) {
-    vector<Ksiazka_W_Obiegu> ksiazkiWObiegu = db.getKsiazkiWObiegu();
+	cout << "Ksiazki o wygaslych terminach wypozyczen:" << endl;
 
-    cout << "Książki o wygasłych terminach wypożyczeń:" << endl;
+	for (auto* ksiazkaWObiegu : *ksiazkiWypozyczone) {
+		if (!ksiazkaWObiegu->get_zwrot() && ksiazkaWObiegu->get_koniec_czasu_wyporzczenia() < time(nullptr)) {
+			cout << "ISBN: " << ksiazkaWObiegu->get_ISBN() << endl;
+			cout << "Czas wypozyczenia: " << ksiazkaWObiegu->get_czas_wyporzczenia() << endl;
+			cout << "Czas zakonczenia wypozyczenia: " << ksiazkaWObiegu->get_koniec_czasu_wyporzczenia() << endl;
+			cout << endl;
+		}
+	}
+}
 
-    for (const auto& ksiazkaWObiegu : ksiazkiWObiegu) {
-        if (ksiazkaWObiegu.getIdOsoby() == getId() && ksiazkaWObiegu.isCzasWypozyczeniaWygasl()) {
-            cout << "ISBN: " << ksiazkaWObiegu.getISBN() << endl;
-            cout << "Czas wypożyczenia: " << ksiazkaWObiegu.getCzasWypozyczenia() << endl;
-            cout << "Czas zakończenia wypożyczenia: " << ksiazkaWObiegu.getCzasZakonczenia() << endl;
-            cout << endl;
-        }
-    }
+void Czytelnik::wyswietlWyporzyczoneKsiazki(Database& db)
+{
+	for (Ksiazka_W_Obiegu* ksiazkaWObiegu : *ksiazkiWypozyczone) {
+		if (!ksiazkaWObiegu->get_zwrot()) {
+			cout << "ISBN: " << ksiazkaWObiegu->get_ISBN() << endl;
+			cout << "Czas wypożyczenia: " << ksiazkaWObiegu->get_czas_wyporzczenia() << endl;
+			cout << "Czas zakończenia wypożyczenia: " << ksiazkaWObiegu->get_koniec_czasu_wyporzczenia() << endl;
+			cout << endl;
+		}
+	}
+
 }
